acaec25ed72147baea85a65ec26528bd0ee027f7
olivier-goy <olivier.goy.37@gmail.com>
2023-07-12 17:03:38 +0200

    create function header and import inner index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,14 @@ import './Styles/index.css';
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
 import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <header>
+      <Header />
+    </header>
     <RouterProvider router={routes} />
     <footer>
       <Footer />
