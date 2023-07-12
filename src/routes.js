a835e9cb00e812e6e497b71afdaa3331386aaf59
olivier-goy <olivier.goy.37@gmail.com>
2023-07-12 16:10:35 +0200

    create file route for routing inner project

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Error from "./Pages/404/Error";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/*",
+        element: <Error />
+    }
+])
+
+export default routes
