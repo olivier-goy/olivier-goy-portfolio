31c77663d5469970b328d1a7a6038b391d48520e
olivier-goy <olivier.goy.37@gmail.com>
2023-07-12 13:49:38 +0200

    modified import inner file index.jsx and move index.css inner folder Styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Styles/index.css';
+import Home from './Pages/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
