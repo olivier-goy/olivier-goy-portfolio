726ee10a5e724f3216dc7fb4bbaf72c7bdec4db7
olivier-goy <olivier.goy.37@gmail.com>
2023-07-12 17:01:44 +0200

    create function footer and import inner index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Styles/index.css';
-import { RouterProvider } from "react-router-dom"
-import routes from "./routes"
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes";
+import Footer from './Components/Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={routes} />
+    <footer>
+      <Footer />
+    </footer>
   </React.StrictMode>
 );
 
