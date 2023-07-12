65cdc8abc3763eee2c04c9f2e2690aa0436764aa
olivier-goy <olivier.goy.37@gmail.com>
2023-07-12 18:48:46 +0200

    add style for link navigation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,31 @@
 import "./Header.css";
+import icon from "../../Assets/iconHeader.png"
 
 function Header() {
     return (
         <div className="header">
             <div className="iconHeader">
-
+                <img src={ icon } alt="Icon site" />
             </div>
             <div className="navHeader">
-
+                <div>
+                    ACCUEIL
+                </div>
+                <div>
+                    COMPETENCE
+                </div>
+                <div>
+                    A-PROPOS
+                </div>
+                <div>
+                    REALISATIONS
+                </div>
+                <div>
+                    CV
+                </div>
+                <div>
+                    CONTACT
+                </div>
             </div>
         </div>
     )
