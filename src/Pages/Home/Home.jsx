fc49a20639fc4f311ad5007dea5800bb7be10ef7
olivier-goy <olivier.goy.37@gmail.com>
2023-07-12 16:39:42 +0200

    add function Card and import inner home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import './Home.css';
+import Card from '../../Components/Card/Card';
 
 function Home() {
   return (
     <div className="App">
       <h1>Home</h1>
+      <Card />
     </div>
   );
 }
