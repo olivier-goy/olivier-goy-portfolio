f21012f44ca7c6ff243984c61ddf5135a98fec95
olivier-goy <olivier.goy.37@gmail.com>
2023-07-12 16:57:21 +0200

    create function for component carouselProjectImage

---
+++ src/Components/Carousel/CarouselProject/CarouselProject.jsx
@@ -0,0 +1,11 @@
+import "./CarouselProject.css";
+
+function CarouselProject() {
+    return (
+        <div>
+            <h1>carousel project</h1>
+        </div>
+    )
+}
+
+export default CarouselProject
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import './Home.css';
 import Card from '../../Components/Card/Card';
+import CarouselProject from "../../Components/Carousel/CarouselProject/CarouselProject"
 
 function Home() {
   return (
     <div className="App">
       <h1>Home</h1>
       <Card />
+      <CarouselProject />
     </div>
   );
 }
 
-export default Home;
+export default Home
