439a9ecbc004451b268517184ddcdda1ef3b0a26
olivier-goy <olivier.goy.37@gmail.com>
2023-07-18 09:49:13 +0200

    reverse code

--- src/index.jsx
+++ src/index.jsx
@@ -6,16 +6,9 @@ import routes from "./routes";
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-
-// );
-
-import { hydrate, render } from "react-dom";
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <BrowserRouter>
       <header>
         <Header />
@@ -25,18 +18,6 @@ if (rootElement.hasChildNodes()) {
     <footer>
       <Footer />
     </footer>
-  </React.StrictMode>, rootElement);
-} else {
-  render(<React.StrictMode>
-    <BrowserRouter>
-      <header>
-        <Header />
-      </header>
-    </BrowserRouter>
-    <RouterProvider router={routes} />
-    <footer>
-      <Footer />
-    </footer>
-  </React.StrictMode>, rootElement);
-}
+  </React.StrictMode>
+);
 
