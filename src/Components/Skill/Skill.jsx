7d3ca104b472062d0139858cafd88b63323dfa9a
olivier-goy <olivier.goy.37@gmail.com>
2023-07-24 17:08:24 +0200

    import logo of Assets folder for Skill.jsx

--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -1,4 +1,8 @@
 import "./Skill.css";
+import logoCode from "../../Assets/code.png";
+import logoFramework from "../../Assets/algorithme.png";
+import logoData from "../../Assets/bases-de-donnees.png";
+import logoTool from "../../Assets/programmation.png";
 
 function Skill() {
 
