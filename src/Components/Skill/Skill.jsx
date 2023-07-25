e9d9dd00624685bebbdfa6536f1cfe4b815eb941
olivier-goy <olivier.goy.37@gmail.com>
2023-07-25 09:08:33 +0200

    create code for component skill and create code style

--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -1,14 +1,14 @@
 import "./Skill.css";
-import logoCode from "../../Assets/code.png";
-import logoFramework from "../../Assets/algorithme.png";
-import logoData from "../../Assets/bases-de-donnees.png";
-import logoTool from "../../Assets/programmation.png";
 
-function Skill() {
+function Skill({logo, title, skillsList, altSkill}) {
 
     return (
-        <div className="skillSection">
-                <h2>MES COMPETENCES</h2>
+        <div className="skill">
+            <img src={logo} alt={altSkill} />
+            <h3>{title}</h3>
+            {skillsList.map((skillList, index) => (
+                <p key={index + skillList}>{skillList}</p>
+            ))}
         </div>
     )
 }
