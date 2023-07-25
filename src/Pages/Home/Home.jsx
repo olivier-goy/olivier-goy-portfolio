075106f49abd77ca4fb7e2e3ce535f0a1f07c0c9
olivier-goy <olivier.goy.37@gmail.com>
2023-07-25 09:07:22 +0200

    move code fixe of the skill as title towards page home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,16 +6,54 @@ import Modal from '../../Components/Modal/Modal';
 import Tag from '../../Components/Tag/Tag';
 import Presentation from '../../Components/Presentation/Presentation';
 import Skill from '../../Components/Skill/Skill';
+import { useEffect, useState } from 'react';
+
+
+
 
 
 function Home() {
+
+  const [isProfilData, setIsProfilData] = useState([])
+
+  useEffect(() => {
+    async function fetchProfil() {
+      try {
+        const response = await fetch('http://localhost:3000/Data/profil.json')
+        const profilData = await response.json()
+        setIsProfilData(profilData)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchProfil()
+  }, [])
+
   return (
     <div className="home">
+      {console.log(isProfilData[0])}
       <section>
-       <Presentation />
+        <Presentation />
       </section>
       <section>
-        <Skill />
+        <div id="skills" className="skillSection">
+          <h2>MES COMPETENCES</h2>
+          <div className="divisionSkill">
+            {
+              isProfilData.map(skillProfil => (
+                skillProfil.skills.map((profilData, index) => (
+                < Skill
+                  key={profilData + index}
+                  logo={profilData.logoSkillUrl}
+                  altSkill={profilData.altSkill}
+                  title={profilData.titleSkill}
+                  skillsList={profilData.listSkills}
+                />
+              ))
+              ))
+            }
+          </div>
+        </div>
       </section>
       <Card />
       <CarouselProject />
