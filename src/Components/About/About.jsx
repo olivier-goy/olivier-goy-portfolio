675db350382472e3dc966dfc9f861255d3795291
olivier-goy <olivier.goy.37@gmail.com>
2023-07-26 15:48:51 +0200

    responsive section about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,11 +1,11 @@
 import "./About.css";
 
-function About({descriptionText, trainningText}) {
+function About({ descriptionText, trainningText }) {
     return (
-        <div className="aboutSection">
+        <div id="abouts" className="aboutSection">
             <h2>A PROPOS DE MOI</h2>
-            <p>{ descriptionText }</p>
-            <p className="aboutLastParagraph">{ trainningText }</p>
+            <p>{descriptionText}</p>
+            <p className="aboutLastParagraph">{trainningText}</p>
         </div>
     )
 }
