6b1ff1511d5e5c93f4ab3e51e0968a6782d0d1f2
olivier-goy <olivier.goy.37@gmail.com>
2023-07-31 08:36:04 +0200

    create page 404

--- src/Pages/404/Error.jsx
+++ src/Pages/404/Error.jsx
@@ -2,8 +2,12 @@ import "./Error.css"
 
 function Error() {
     return (
-        <div>
-            <h1>404</h1>
+        <div className="styleText404">
+            <h1 className="style404">404</h1>
+            <p className="styleTextError">
+                Oups! La page que vous demandez n'existe pas.
+            </p>
+            <a href="/" className="styleTextReturn">Retourner sur la page d'accueil</a>
         </div>
     )
 }
