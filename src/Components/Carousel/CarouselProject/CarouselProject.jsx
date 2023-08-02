0393df39fac4bfaf7b06f02c94687c1d1f6ab371
olivier-goy <olivier.goy.37@gmail.com>
2023-08-02 09:34:04 +0200

    add timer for probleme index on scroll with iphone

--- src/Components/Carousel/CarouselProject/CarouselProject.jsx
+++ src/Components/Carousel/CarouselProject/CarouselProject.jsx
@@ -10,14 +10,26 @@ function CarouselProject({ realizations }) {
     const [windowSize, setWindowSize] = useState(getSize)
 
     function getSize() {
-        return window.innerWidth
+        return window.screen.width
     }
-
+    
+    function debounce(func, delay) {
+        let timer;
+        return function (...args) {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+            func.apply(this, args);
+          }, delay);
+        };
+      }
+    
     useEffect(() => {
         function handleResize() {
             setWindowSize(getSize())
-            setIsIndex(0)
+            debounce(setIsIndex(0), 100)
+            
         }
+
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, [])
