ad34e6ffd200b49a89fb5c3e677e7c45f09c6871
olivier-goy <olivier.goy.37@gmail.com>
2023-12-24 21:27:10 +0100

    commit for end day

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,9 +1,53 @@
+import { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import imageProfil from "../../Assets/Photo-de-profil.webp";
 import "./Banner.css"
 
 function Presentation() {
+    const lines = [
+        "Bonjour et bienvenu,",
+        "Je suis Olivier GOY",
+        "Développeur web fullstack"
+    ];
+
+    const [text, setText] = useState('');
+    const [lineIndex, setLineIndex] = useState(0);
+    const [charIndex, setCharIndex] = useState(0);
+    const typingRef = useRef(null);
+
+
+    useEffect(() => {
+        typingRef.current = setInterval(() => {
+            if (lineIndex === lines.length && charIndex === 0) {
+                clearInterval(typingRef.current);
+                return;
+            }
+
+            const currentLine = lines[lineIndex];
+            const currentChar = currentLine.charAt(charIndex);
+
+            if (lineIndex >= lines.length - 2) {
+                setText(prevText => prevText + `<span style="color: gray">${currentChar}</span>`);
+            } else {
+                setText(prevText => prevText + currentChar);
+            }
+
+            setCharIndex(prevCharIndex => {
+                let newIndex = prevCharIndex + 1;
+                if (newIndex > currentLine.length) {
+                    newIndex = 0;
+                    setLineIndex(prevLineIndex => prevLineIndex + 1);
+                    setText(prevText => prevText + '<br />');
+                }
+                return newIndex;
+            });
+        }, 50);
+
+        return () => clearInterval(typingRef.current);
+        // eslint-disable-next-line
+    }, [lineIndex, charIndex]);
+
     return (
         <div className="banner">
             <div className="innerBanner">
@@ -11,12 +55,15 @@ function Presentation() {
                     <img src={imageProfil} alt="Représentation profil Olivier GOY développeur web" />
                 </div>
                 <div className="sectionTitle">
-                    <h1>
-                        <span>Bonjour et bienvenue,</span>
-                        <br />
-                        <span className="spanTitle">Je suis Olivier GOY</span>
-                        <br />
-                        <span className="spanTitle">Développeur Web Fullstack</span>
+                    <h1
+                        dangerouslySetInnerHTML={{ __html: text }}
+                        style={{
+                            width: '10px',
+                            height: '20px',
+                            background: 'black',
+                            animation: 'blink 1s step-end infinite',
+                        }}
+                    >
                     </h1>
                 </div>
             </div>
